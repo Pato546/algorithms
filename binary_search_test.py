405c2ea02c7990dab22253e12ae7dc7bf51813e9
Pato <boatengpato.pb@gmail.com>
2021-06-22 02:51:43 -0400

    Unittest for the python binary search algorithm

---
+++ binary_search_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from binary_search import binary_search
+import unittest
+
+
+class TestBinarySearch(unittest.TestCase):
+    
+    def test_basic(self):
+        self.assertEqual(binary_search([1, 2, 3, 4, 5, 6], 4), 3)
+        self.assertEqual(binary_search([1, 2, 3, 4, 5, 6], 6), 5)
+        self.assertEqual(binary_search([1, 2, 3, 4, 5, 6], 3), 2)
+        self.assertEqual(binary_search([1, 2, 3, 4, 5, 6], 1), 0)
+
+    def test_empty(self):
+        self.assertEqual(binary_search([1, 2, 3, 4, 5, 6], 7), None)
+
+    def __str__(self):
+        return 'TestBinarySearch'
+
+unittest.main()
