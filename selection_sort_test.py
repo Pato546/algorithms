90dfbd500b4657fa82f9d58306d9300862b716ec
Pato <boatengpato.pb@gmail.com>
2021-08-13 09:21:02 -0400

    Minor change

--- selection_sort_test.py
+++ selection_sort_test.py
@@ -11,7 +11,7 @@ class SelectionSort(unittest.TestCase):
         self.assertEqual(selection_sort([1,5,2,3,4,8], reverse=True), [8,5,4,3,2,1])
 
     def test_array_empty(self):
-        self.assertEqual(selection_sort([]), 'cannot sort an empty list')
+        self.assertEqual(selection_sort([]), [])
 
     def test_array_duplicate(self):
         self.assertEqual(selection_sort([2, 9, 5, 2, 6, 5, 8, 4, 2]), [2, 2, 2, 4, 5, 5, 6, 8, 9])
