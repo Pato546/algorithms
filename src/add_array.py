7e86441f78bd8157de0319bfbbb86d0585b5daaf
Pato <boatengpato.pb@gmail.com>
2022-10-05 16:39:36 +0000

    Project restructuring

--- src/add_array.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def sum_arr(arr:list) -> int:
-
-    if len(arr) == 0:
-        return 0
-
-    return arr[0] + sum_arr(arr[1:])
--- src/count_arr.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def count_arr(arr:list) -> int:
-    
-    if len(arr) == 0:
-        return 0
-
-    return 1 + count_arr(arr[1:])
