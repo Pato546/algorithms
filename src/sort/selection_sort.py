60f8dee1f450713a83aede700b9a1a7ed5534bec
Pato <boatengpato.pb@gmail.com>
2022-11-01 09:31:28 +0000

    Change the selection sort algorithm

--- src/sort/selection_sort.py
+++ src/sort/selection_sort.py
@@ -5,16 +5,19 @@ def selection_sort(arr: list) -> list:
     """Sorts a given list using selection sort algorithm
 
     Examples:
-        >>> selection_sort([8,9, 6, 7, 4, 5, 1, 3, 2])
+        >>> selection_sort([8, 9, 6, 7, 4, 5, 1, 3, 2])
         [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     Args:
         arr (list): list to be sorted
 
     """
-    for i in range(len(arr)):
+
+    arr_len = len(arr)
+
+    for i in range(arr_len):
         min_idx = i
-        for j in range(i, len(arr)):
+        for j in range(i, arr_len):
             if arr[j] < arr[min_idx]:
                 min_idx = j
 
